e908958c67ac0a39bfdf6ead3d50a92b2c62d85c
sanjayvp <svpselenium@gmail.com>
2020-10-03 18:32:43 +0530

    Commiting project for first time

---
+++ Demo1/src/test/java/demoPackage/DemoClass1.java
@@ -0,0 +1,20 @@
+package demoPackage;
+
+import org.testng.annotations.Test;
+
+public class DemoClass1 {
+
+	@Test
+	public void m1() {
+		// TODO Auto-generated method stub
+		System.out.println("this is m1 method");
+
+	}
+	@Test
+	public void m2() {
+		// TODO Auto-generated method stub
+		System.out.println("this is m2 method");
+
+	}
+}
+
